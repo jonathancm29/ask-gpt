0c4025cef7ea185ac12fa77c9732c39d1b34082c
Jonathan Cardenas <jonathan.cardenasm29@gmail.com>
2023-03-23 19:55:00 -0500

    add new model for gpt 4

--- src/consts/consts.ts
+++ src/consts/consts.ts
@@ -1,4 +1,4 @@
 export const MODELS_OPENAI = {
   completation: ['text-davinci-003', 'text-davinci-002', 'code-davinci-002'],
-  chat: ['gpt-3.5-turbo', 'gpt-3.5-turbo-0301', 'gpt-4']
+  chat: ['gpt-3.5-turbo', 'gpt-3.5-turbo-0301', 'gpt-4-0314']
 } as const
