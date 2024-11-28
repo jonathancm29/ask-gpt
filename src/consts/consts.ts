43153eb2ae6fb9ac2ff62e87137b70158b2e3f3d
jonathancm29 <50557324+jonathancm29@users.noreply.github.com>
2024-11-27 23:12:18 -0500

    Update consts.ts

--- src/consts/consts.ts
+++ src/consts/consts.ts
@@ -1,4 +1,4 @@
 export const MODELS_OPENAI = {
-  completation: ['text-davinci-003', 'text-davinci-002', 'code-davinci-002'],
-  chat: ['gpt-3.5-turbo', 'gpt-3.5-turbo-0301', 'gpt-4-0314']
+  completation: ['text-davinci-003', 'text-davinci-002', 'code-davinci-002','gpt-4o'],
+  chat: ['gpt-3.5-turbo', 'gpt-3.5-turbo-0301', 'gpt-4-0314','gpt-4o']
 } as const
